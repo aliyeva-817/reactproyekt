0adb62e2383ea2f4ddfabe2cde408e1cfe0fa88b
valide2004 <validavf-ap217@code.edu.az>
2025-04-16 12:57:33 +0400

    first commit

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import App from '../../App'
 const About = () => {
+
+    
+let basket=JSON.parse(localStorage.getItem('sebet')) || []
+    
+      
   return (
-    <div>About</div>
+    <div>  {basket && basket.map( item => {
+        return <div className='card'>
+          <img src={item.image} alt="" />
+          <span>{item.price}</span>
+          
+        </div>
+      })}</div>
   )
 }
 
